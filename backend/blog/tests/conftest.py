555aa56624e9b50128f381c7aed59cc00784365c
Amir Hamidi <Amirarsalanh2001@gmail.com>
2026-06-09 08:52:10 +0330

    Fix UserFactory: skip_postgeneration_save to avoid extra save on password set

--- backend/blog/tests/conftest.py
+++ backend/blog/tests/conftest.py
@@ -14,6 +14,7 @@ User = get_user_model()
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
+        skip_postgeneration_save = True
 
     email = factory.Sequence(lambda n: f"user{n}@example.com")
     password = factory.PostGenerationMethodCall("set_password", "testpass123!")
